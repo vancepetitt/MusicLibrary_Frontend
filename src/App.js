4904a3a592b1933c886eff0f692acdb2a6a492c8
vance.petitt <vance.petitt@gmail.com>
2022-03-11 15:39:47 -0600

    Create newSongForm with hook and event listener functions.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import NewSongForm from "./Components/NewSongForm/NewSongForm";
+
 
 function App() {
   return (
     <div>
       <h1>Music Library</h1>
+      <NewSongForm />
     </div>
   );
 }
---
+++ src/Components/NewSongForm/NewSongForm.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+
+const NewSongForm = (props) => {
+
+    const [title, setTitle] = useState(''); //hook functions to use in creation of the form function
+    const [artist, setArtist] = useState('');
+    const [album, setAlbum] = useState('');
+    const [releaseDate, setReleaseDate] = useState('');
+    const [genre, setGenre] = useState('');
+
+    //listener function to take the form data and create a new song when even (submit). Listing the hook variables gathers the input data for use.
+    function handleSubmit(event) {
+        let newSong = {
+            title: title,
+            artist: artist,
+            album: album,
+            releaseDate: releaseDate,
+            genre: genre,
+        };
+        console.log(newSong)
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <label for='title'>Title: </label> {/*labels the input box */}
+            <br></br>
+            <input type='text' value={title} onChange={(event) => setTitle(event.target.value)}/>
+            <br></br>
+            <label for='postText'>Post: </label>
+            <br></br>
+            <input type='text' value={artist} onChange={(event) => setArtist(event.target.value)}/>
+            <br></br>
+            <label for='title'>Album: </label>
+            <br></br>
+            <input type='text' value={album} onChange={(event) => setAlbum(event.target.value)}/>
+            <br></br>
+            <label for='title'>Release Date: </label>
+            <br></br>
+            <input type='text' value={releaseDate} onChange={(event) => setReleaseDate(event.target.value)}/>
+            <br></br>
+            <label for='title'>Genre: </label>
+            <br></br>
+            <input type='text' value={genre} onChange={(event) => setGenre(event.target.value)}/>
+            <br></br>
+            <br></br>
+            <button type='submit'>Submit</button>
+        </form>
+    );
+}
+
+export default NewSongForm
